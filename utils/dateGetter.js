d73a10b811dee456244cf1976a3f92959b2f5a38
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-05-21 02:02:59 -0400

    added dependencies

---
+++ utils/dateGetter.js
@@ -0,0 +1,10 @@
+
+   
+const formatDate = (date) => {
+    const formatDate = (date)=>{
+        let formatted_date = (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear()
+         return formatted_date;
+        }
+}
+
+module.exports = formatDate
